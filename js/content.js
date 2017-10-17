4d4d8a2cc0f57aae323b5f06b7bdfbf52fa0b108
Bryan Tran <bryanvantran@ucla.edu>
2017-10-16 22:29:12 -0700

    adding files

---
+++ js/content.js
@@ -0,0 +1,75 @@
+function setSelectedValue(selectObj, valueToSet) {
+    for (var i = 0; i < selectObj.options.length; i++) {
+        if (selectObj.options[i].text== valueToSet) {
+            selectObj.options[i].selected = true;
+            return;
+        }
+    }
+}
+
+function autofill(info) {
+  var inputs = document.querySelectorAll('input:not([type=submit])');
+  inputs.forEach(function(element) {
+    // get the previous sibling, use that to find the field label
+    var prev_sibling = element.previousElementSibling;
+
+    // use field label to query dict for personal info
+    if (prev_sibling) {
+      var label_text = prev_sibling.innerHTML.toLowerCase();
+
+      // fill in info
+      var value = info[label_text];
+      element.value = value;
+
+      // console.log(label_text, value);
+    }
+  });
+
+  // setting dropdown values
+  var selects = document.querySelectorAll('select');
+  selects.forEach(function(element) {
+
+    // get the previous sibling, use that to find the field label
+    var prev_sibling = element.previousElementSibling;
+
+    // use field label to query dict for personal info
+    if (prev_sibling) {
+      // check if prev sibling is a select
+      if (prev_sibling.tagName.toLowerCase() === 'select') {
+        var value = info['exp_year'];
+        setSelectedValue(element, value);
+      }
+      else {
+        var label_text = prev_sibling.innerHTML.toLowerCase();
+        if (label_text === 'exp. date') {
+          var value = info['exp_month'];
+        }
+        else {
+          var value = info[label_text];
+        }
+        // fill in info
+        setSelectedValue(element, value);
+
+        //  console.log(label_text, value);
+      }
+    }
+  })
+
+  // pressing checkbox
+  try {
+    var terms_btn = d.getElementById('order_terms').click();
+  }
+  catch(err) {
+    console.log("error filling out checkbox");
+  }
+}
+
+function autofillOnClick(key, info) {
+  $(document).keypress(function(event) {
+    if (String.fromCharCode(event.which) == key) {
+      autofill(info)
+    }
+  });
+}
+
+autofillOnClick('`', BILLING_INFO);
---
+++ js/info.js
@@ -0,0 +1,16 @@
+var BILLING_INFO = {
+    "name": "First Last",
+    "email": "test@gmail.com",
+    "tel": "7141234567",
+    "address": "12345 Test St.",
+    "address 2": "",
+    "zip": "90024",
+    "city": "Los Angeles",
+    "state": "CA", // 2 character abbreviation
+    "country": "USA", // USA or CANADA
+    "number": "4242424242424242",
+    "exp_month": "11", // 2 digits
+    "exp_year": "2018", // 4 digits
+    "cvv": "123"
+}
+  
